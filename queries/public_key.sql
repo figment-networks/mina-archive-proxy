6de08dc44d198fcdaf5cc9dd613615dd79b318db
Dan Sosedoff <dan.sosedoff@gmail.com>
2021-01-27 15:01:01 -0600

    Rename public key value -> id

--- queries/public_key.sql
+++ queries/public_key.sql
@@ -1,5 +1,5 @@
 SELECT
-  value,
+  value AS id,
   (SELECT COUNT(1) FROM user_commands WHERE source_id = public_keys.id) AS commands_sent,
   (SELECT COUNT(1) FROM user_commands WHERE receiver_id = public_keys.id) AS commands_received
 FROM
